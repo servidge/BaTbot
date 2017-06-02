0d0b4a4418a3c3d860feec615519fcab2ef2b736
Sebastian <news.sb@web.de>
2017-06-03 00:23:14 +0200

    Lastname from user_list
    Lastname from user_list

--- batbot/admlistuser.sh
+++ batbot/admlistuser.sh
@@ -9,7 +9,8 @@ if [[ $(grep -c -x "$1" $ALLSUPUSER) -eq 1 ]]; then
 	for USER in $(cat $ALLUSER); do
 		ID=$(grep $USER $USERLIST | awk -F":" '{printf $1}')
 		NAME=$(grep $USER $USERLIST | awk -F":" '{printf $2}')
-		echo "TelegramID: $ID - User: $NAME"
+		LAST=$(grep $USER $USERLIST | awk -F":" '{printf $3}')
+		echo "TelegramID: $ID - User: $NAME - Lastname: $LAST"
 	done
 else
 	echo "You are not allowed to view the user list"
